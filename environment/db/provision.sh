fa0f8f190ebced142540da5b337bd11a0cc853e5
Ash Isbitt <AIsbitt@tcadmins-air-2.home>
2020-03-24 15:44:22 +0000

    update with provided files

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+# be careful of these keys, they will go out of date
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# sudo apt-get install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+# remoe the default .conf and replace with our configuration
+sudo rm /etc/mongod.conf
+sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
 
-sudo systemctl start mongod
+# if mongo is is set up correctly these will be successful
+sudo systemctl restart mongod
 sudo systemctl enable mongod
